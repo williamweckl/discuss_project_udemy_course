c7db60f02a1ba3118497fa46e8a3d24c90bbca99
William Weckl <william.weckl@gmail.com>
2017-03-20 22:04:12 -0300

    update and delete

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -25,4 +25,34 @@ defmodule Discuss.TopicController do
         render conn, "new.html", changeset: changeset
     end
   end
+
+  def edit(conn, %{"id" => id}) do
+    topic = Repo.get!(Topic, id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => id, "topic" => topic_params}) do
+    topic = Repo.get!(Topic, id)
+    changeset = Topic.changeset(topic, topic_params)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: topic
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    Repo.get!(Topic, id)
+      |> Repo.delete!
+
+    conn
+      |> put_flash(:info, "Topic Deleted")
+      |> redirect(to: topic_path(conn, :index))
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,9 +17,7 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", TopicController, :index
-    get "/topics", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
